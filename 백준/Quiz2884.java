a4624e3a38fde3d9b5fca055e10e49e9b4ddd99c
천운령 <cur@plani.co.kr>
2024-03-20 08:59:23 +0900

    알람 시계

---
+++ 백준/Quiz2884.java
@@ -0,0 +1,22 @@
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Quiz2884 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+
+        LocalTime localTime = LocalTime.of(hour, minute);
+        LocalTime resultTime = localTime.minusMinutes(45);
+
+        System.out.println(resultTime.getHour() + " " + resultTime.getMinute());
+
+        sc.close();
+
+    }
+
+}
