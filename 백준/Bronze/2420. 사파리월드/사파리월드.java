4219ae121934cc141955863d8be6dd793f7553f0
천운령 <leyng7@gmail.com>
2024-03-24 22:38:02 +0900

    [Bronze V] Title: 사파리월드, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long result = Math.abs(Long.parseLong(st.nextToken()) - Long.parseLong(st.nextToken()));
+        bw.write(String.valueOf(result));
+
+        br.close();
+        bw.close();
+
+    }
+
+
+}
