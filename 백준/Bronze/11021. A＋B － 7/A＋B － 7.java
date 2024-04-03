6f5c90bd75017668a68783d09ece201200f5d364
천운령 <leyng7@gmail.com>
2024-04-03 22:56:09 +0900

    [Bronze V] Title: A+B - 7, Time: 156 ms, Memory: 16092 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + (i + 1) + ": " + (a + b));
+            bw.newLine();
+        }
+
+        br.close();
+        bw.close();
+    }
+
+}
