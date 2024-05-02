e7b2b84abd4366f19477fb519b7c78ab54828842
천운령 <leyng7@gmail.com>
2024-05-02 20:16:21 +0900

    [Bronze V] Title: 아!, Time: 212 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.next().length() >= sc.next().length() ? "go" : "no");
+
+    }
+
+}
