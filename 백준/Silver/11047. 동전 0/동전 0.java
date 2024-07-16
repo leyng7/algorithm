9a36d6b5a5c68cb3a3b06feb23449be10c9489e2
천운령 <leyng7@gmail.com>
2024-07-16 20:01:12 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            int[] coins = new int[n];
+            for (int i = 0; i < n; i++) {
+                coins[i] = Integer.parseInt(br.readLine());
+            }
+
+            int count = 0;
+            for (int i = n - 1; i >= 0; i--) {
+                if (coins[i] <= k) {
+                    count += (k / coins[i]);
+                    k %= coins[i];
+                }
+            }
+
+            bw.write(String.valueOf(count));
+            bw.flush();
+        }
+    }
+}
